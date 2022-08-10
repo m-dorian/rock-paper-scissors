92f638f8035e1d850a9fafe6b599a477709bb58c
macridorian <macridorian9@gmail.com>
2022-08-10 19:48:41 +0300

    Created getPlayerChoice function

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 function getRandomInteger (min, max) { 
     return Math.floor(Math.random() * (max - min + 1)) + min;
-} 
+}
 function getComputerChoice() { 
     let computerChoice = getRandomInteger(1, 3);  
     switch(computerChoice) { 
         case 1: 
-        computerChoice = "Rock"; 
+        computerChoice = "ROCK"; 
         break; 
         case 2: 
-        computerChoice = "Paper"; 
+        computerChoice = "PAPER"; 
         break; 
         case 3: 
-        computerChoice = "Scissors"; 
+        computerChoice = "SCISSORS"; 
         break;
     }
     return computerChoice;
 } 
-alert(getComputerChoice());
+function getPlayerChoice() { 
+    let tempPlayerChoice = prompt("What weapon do you select? Rock, paper or scissors?!");
+    let playerChoice = tempPlayerChoice.toUpperCase(); 
+     if(playerChoice === "ROCK" || playerChoice === "PAPER" || playerChoice === "SCISSORS") { 
+        alert("Valid! You can play the game!")
+    } else alert("Invalid! Try again") 
+    return playerChoice;
+    
+}
+getPlayerChoice();
