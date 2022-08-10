62ffc0b2d7355ea425d978dd77b4049773522f27
macridorian <macridorian9@gmail.com>
2022-08-10 18:54:41 +0300

    Created getComputerChoice function

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function getRandomInteger (min, max) { 
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+} 
+function getComputerChoice() { 
+    let computerChoice = getRandomInteger(1, 3);  
+    switch(computerChoice) { 
+        case 1: 
+        computerChoice = "Rock"; 
+        break; 
+        case 2: 
+        computerChoice = "Paper"; 
+        break; 
+        case 3: 
+        computerChoice = "Scissors"; 
+        break;
+    }
+    return computerChoice;
+} 
+alert(getComputerChoice());
